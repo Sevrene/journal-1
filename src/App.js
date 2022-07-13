f3db55f19209f0d154704eff0d7da7eb8c2d20d6
Sevrene <Dowd_05@hotmail.com>
2022-07-13 16:30:03 -0600

    Test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Section from './Section';
 const text1 = "I wanted to use this journal as a way of expressing my thoughts in a branching manner as they came to me and chose to represent them through this site as a means of connecting my different ideas, questions, and overall thoughts"
 function App() {
   return (
-    <Section text={text1} content={<Section text="Text 2"/>}/>
+    <div class="App">
+      <Section text={text1} content={
+        <>
+        <Section text="Text 2" />
+        <Section text="Text 3" />
+        </>}
+      />
+    </div>
   );
 }
 
