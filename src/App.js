82e71a21912ffc2f9cd86df2cc7ff9c9a103999b
Sevrene <Dowd_05@hotmail.com>
2022-07-13 15:50:18 -0600

    Test

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import Collapse from '@mui/material/Collapse';
 
 function App() {
   const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
   return (
     <>
       <Button variant="outlined" onClick={() => setShow(prev => !prev)}>Test</Button>
         <Collapse in={show}>
-          <Button variant="outlined" onClick={() => setShow(prev => !prev)}>Test</Button>
-          <Collapse in={show}>
+          <Button variant="outlined" onClick={() => setShow2(prev => !prev)}>Test</Button>
+          <Collapse in={show2}>
           <Button variant="outlined" onClick={() => setShow(prev => !prev)}>Test</Button>
           <Collapse in={show}>
           <Button variant="outlined" onClick={() => setShow(prev => !prev)}>Test</Button>
