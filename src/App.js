cec3c57f1c2233a37f631b370bb43c8cd8a41cc4
Sevrene <Dowd_05@hotmail.com>
2022-07-13 16:24:56 -0600

    Fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Section from './Section';
 
+const text1 = "I wanted to use this journal as a way of expressing my thoughts in a branching manner as they came to me and chose to represent them through this site as a means of connecting my different ideas, questions, and overall thoughts"
 function App() {
   return (
-    <Section text="Text 1" content={<Section text="Text 2"/>}/>
+    <Section text={text1} content={<Section text="Text 2"/>}/>
   );
 }
 
