265a042ffc4e51bf7d34167eef0a2a28af2a5cdf
Sevrene <Dowd_05@hotmail.com>
2022-07-13 16:19:43 -0600

    CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Section from './Section';
 
 function App() {
   return (
-    <Section name="Text 1" content={<Section name="Text 2"/>}/>
+    <Section text="Text 1" content={<Section text="Text 2"/>}/>
   );
 }
 
