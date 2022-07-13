cc556a483f2857c4377365262e5cebc7eba13166
Sevrene <Dowd_05@hotmail.com>
2022-07-13 14:55:40 -0600

    Initial Test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/material/Menu';
 
 function App() {
   return (
@@ -17,6 +26,25 @@ function App() {
         >
           Learn React
         </a>
+        <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
       </header>
     </div>
   );
